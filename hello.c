bb83f5607fb92da058e69af5e51af26649dbcefb
zemation <rbrandon@zemation.com>
2017-05-15 11:11:29 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*********************************
+ - cs50 Problem Set 1 hello.c
+ A simple C program to print 
+ hello, world to the screen
+**********************************/
+
+#include <stdio.h>
+//cs50 - pset1 - hello
+
+int main(void)
+{
+    printf("Hello, world!\n");//prints Hello World and ends line
+}
